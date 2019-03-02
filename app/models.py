105cafb031141e91f090af1fd90f28e91735d1e6
Hudda <anshulhudda.ssap@gmail.com>
2019-03-02 17:40:16 +0530

    models: Added Semester Class.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Semester(models.Model):
+
+	CHOICES = (
+			(1, 'First'),
+			(2, 'Second'),
+			(3, 'Third'),
+			(4, 'Fourth'),
+			(5, 'Fifth'),
+			(6, 'Sixth'),
+			(7, 'Seventh'),
+			(8, 'Eigth')
+		)
+
+	semester = models.IntegerField(default = 0, choices=CHOICES)
+
+	def __str__(self):
+		return str(self.semester)
